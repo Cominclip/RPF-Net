75e5ae1e5d28039db7551b3279ef8cb9de643fca
Comin <77779175+Cominclip@users.noreply.github.com>
2023-05-26 16:37:29 +0800

    Update DQN.py

--- models/DQN.py
+++ models/DQN.py
@@ -24,13 +24,16 @@ class Net(nn.Module):
     def __init__(self):                                                        
         super(Net, self).__init__()                                            
 
-        self.fc1 = nn.Linear(N_STATES, 50)                                      
-        self.fc1.weight.data.normal_(0, 0.1)                                   
-        self.out = nn.Linear(50, N_ACTIONS)                                     
+        self.fc1 = nn.Linear(N_STATES, 128)                                      
+        self.fc1.weight.data.normal_(0, 0.1)     
+        self.fc2 = nn.Linear(128, 64)  
+        self.fc2.weight.data.normal_(0, 0.1)  
+        self.out = nn.Linear(64, N_ACTIONS)                                     
         self.out.weight.data.normal_(0, 0.1)                                  
 
     def forward(self, x):                                                       
-        x = F.relu(self.fc1(x))                                                
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         actions_value = self.out(x)                                             
         return actions_value                                                   
 
